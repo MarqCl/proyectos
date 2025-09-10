aed366851db7ad259361f1ef10542765f23d6f60
MarqCl <MarqCLcanolopez@gmail.com>
2025-09-10 12:29:46 -0600

    Add files via upload

---
+++ Sistema de ventas marcos y julissa/Sistema de ventas marcos y julissa/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Sistema_de_ventas_marcos_y_julissa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelpassword = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtinfo = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(67, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Debes iniciar sesión para usar el sistema";
+            // 
+            // txtname
+            // 
+            this.txtname.BackColor = System.Drawing.Color.LightCyan;
+            this.txtname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtname.Location = new System.Drawing.Point(104, 87);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(190, 20);
+            this.txtname.TabIndex = 4;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.BackColor = System.Drawing.Color.LightCyan;
+            this.txtpassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtpassword.Location = new System.Drawing.Point(105, 174);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(190, 20);
+            this.txtpassword.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(18, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "nombre:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.DarkCyan;
+            this.groupBox1.Controls.Add(this.labelpassword);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtpassword);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtname);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Location = new System.Drawing.Point(12, 162);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(379, 330);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ingrese los datos de usuario";
+            // 
+            // labelpassword
+            // 
+            this.labelpassword.AutoSize = true;
+            this.labelpassword.BackColor = System.Drawing.Color.Teal;
+            this.labelpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelpassword.ForeColor = System.Drawing.Color.Orange;
+            this.labelpassword.Location = new System.Drawing.Point(101, 216);
+            this.labelpassword.Name = "labelpassword";
+            this.labelpassword.Size = new System.Drawing.Size(212, 18);
+            this.labelpassword.TabIndex = 8;
+            this.labelpassword.Text = "ingrese la contraseña correcta.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(6, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "contraseña:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCyan;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Image = global::Sistema_de_ventas_marcos_y_julissa.Properties.Resources.usuario__2_;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(116, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iniciar sesion";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.groupBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.panel1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(416, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(259, 479);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Sistema_de_ventas_marcos_y_julissa.Properties.Resources._1694041663679;
+            this.pictureBox2.Location = new System.Drawing.Point(81, 19);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 70);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtinfo);
+            this.panel1.Location = new System.Drawing.Point(6, 96);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(247, 111);
+            this.panel1.TabIndex = 4;
+            // 
+            // txtinfo
+            // 
+            this.txtinfo.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.txtinfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtinfo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtinfo.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.txtinfo.Location = new System.Drawing.Point(14, 12);
+            this.txtinfo.Multiline = true;
+            this.txtinfo.Name = "txtinfo";
+            this.txtinfo.Size = new System.Drawing.Size(202, 1072);
+            this.txtinfo.TabIndex = 4;
+            this.txtinfo.Text = resources.GetString("txtinfo.Text");
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCyan;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Image = global::Sistema_de_ventas_marcos_y_julissa.Properties.Resources.salir_alt__1_;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.Location = new System.Drawing.Point(69, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 79);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Salir";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Sistema_de_ventas_marcos_y_julissa.Properties.Resources.avatar;
+            this.pictureBox1.Location = new System.Drawing.Point(128, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(678, 504);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sistema de ventas - Inicio de sesión";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelpassword;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtinfo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Sistema de ventas marcos y julissa/Sistema de ventas marcos y julissa/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_ventas_marcos_y_julissa
+{
+    public partial class Form1 : Form
+    {
+        //declaramos variables que contienen nobre y contraseña correctos
+        string nombre1 = "marcos";
+        string nombre2 = "julissa";
+        string password1 = "libro123";
+        string password2 = "libro1234";
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            //iniciaizo el labelpassword como invisible
+            labelpassword.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //obtenemos el texto de los textbox
+            string nombreIngresado = txtname.Text;
+            string passwordIngresado = txtpassword.Text;
+            //comprobamos que los campos están vacios
+            if (nombreIngresado == "" || passwordIngresado == "")
+            {
+                //mostramos un mensaje de error
+                MessageBox.Show("Debes ingresar datos", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //comprobamos si el nobre y la contraseña son correctas
+                if((nombreIngresado==nombre1 && passwordIngresado== password1) || (nombreIngresado==nombre2 && passwordIngresado==password2))
+                {
+                    //mostramos un mensaje de exito
+                    MessageBox.Show("se inició sesión correctaente", "exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    
+                    //ocultamos el labelpassword
+                    labelpassword.Visible = passwordIngresado != ""; /* Lo que hace es asignar un valor
+                                  booleano (verdadero o falso) a la propiedad Visible 
+                                  del labelpassword, que determina si se muestra
+                                  o no en el formulario. El valor booleano depende
+                                  de la comparación que se hace entre el 
+                                  passwordIngresado y una cadena vacía (“”), 
+                                  usando el operador de desigualdad (!=), que devuelve
+                                  verdadero si los valores son distintos y falso si son
+                                  iguales. Es decir, si el passwordIngresado es diferente
+                                  de una cadena vacía, el labelpassword se mostrará, y si
+                                  es igual, se ocultará.*/
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("estás seguro de cerrar el programa?", "ventana de salir", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+        }
+
+        
+
+    }
+}
